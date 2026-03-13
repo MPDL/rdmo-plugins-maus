e681098681597c3ee95bd3f13a13f3da90fa88f6
Laura Bahamon <bahamon@mpdl.mpg.de>
2026-03-13 08:38:34 +0100

    add validate_text_field, validate_file_path and FilePathExtensionValidator

---
+++ rdmo_maus/forms/custom_validators.py
@@ -0,0 +1,73 @@
+import re
+
+from django.utils.deconstruct import deconstructible
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+def validate_text_field(field_name, value, min_length, max_length, not_allowed_pattern, allowed_char_name_str):
+    errors = []
+    
+    matches = re.findall(not_allowed_pattern, value)
+    matches = list(set(matches))
+    if len(matches) > 0:
+        try:
+            index = matches.index(' ')
+            matches[index] = str(_('whitespace'))
+        except:
+            pass
+
+        errors.append(ValidationError(
+            _('{field_name} contains special character(s): "{spec_chars}". Allowed characters are: {allowed_char_name_str}.').format(
+                field_name=field_name,
+                spec_chars='", "'.join(matches),
+                allowed_char_name_str=allowed_char_name_str
+            ),
+            code='invalid'
+        ))
+    
+    if len(value) > max_length:
+        errors.append(ValidationError(
+            _('{field_name} must have at most {max_length} characters (it has {len_value}).').format(
+                field_name=field_name,
+                max_length=max_length,
+                len_value=len(value)
+            ),
+            code='invalid'
+        ))
+
+    if len(value) < min_length:
+        errors.append(ValidationError(
+            _('{field_name} must have at least {min_length} characters (it has {len_value}).').format(
+                field_name=field_name,
+                min_length=min_length,
+                len_value=len(value)
+            ),
+            code='invalid'
+        ))
+
+    if len(errors) > 0:
+        raise ValidationError(errors)
+    
+
+def validate_file_path(value):
+    field_name = _('File path')
+    min_length = 6
+    max_length = 100
+    not_allowed_pattern = f'[^A-Za-z0-9\/\-\_\.]'
+    allowed_char_name_str = _('alphanumeric, slash, hyphen, underscore, and period')
+
+    return validate_text_field(field_name, value, min_length, max_length, not_allowed_pattern, allowed_char_name_str)
+
+
+@deconstructible
+class FilePathExtensionValidator:
+
+    def __init__(self, valid_extension: str):
+        self.valid_extension = valid_extension
+
+    def __call__(self, value):
+        if not value.endswith(self.valid_extension):
+            raise ValidationError(
+                _('File must be in {valid_extension} format.').format(valid_extension=self.valid_extension),
+                code='invalid'
+            )
