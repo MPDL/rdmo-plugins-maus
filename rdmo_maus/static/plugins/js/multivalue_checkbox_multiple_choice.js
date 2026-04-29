7ea3a4e97dd8ab23a2fde64e53541375172d3316
Laura Bahamon <bahamon@mpdl.mpg.de>
2026-04-29 18:41:10 +0200

    add fieldId to uniquely identify widget html tags of MultivalueCheckboxMultipleChoiceField

--- rdmo_maus/static/plugins/js/multivalue_checkbox_multiple_choice.js
+++ rdmo_maus/static/plugins/js/multivalue_checkbox_multiple_choice.js
@@ -1,112 +1,114 @@
-const draggables = document.querySelectorAll('.drag-icon');
-const droppable = document.querySelector('.selected');
-const defaultZone = document.querySelector('.unselected');
+const draggables = document.querySelectorAll('.drag-icon')
+const droppable = document.querySelector('.selected')
+const defaultZone = document.querySelector('.unselected')
 
-function moveChoice(droppable, index, selected) {
-  const choiceBlock = document.getElementById(index);
-  let dragIcon = document.getElementById(`drag_icon_${index}`)
+function moveChoice(droppable, fieldId, index, selected) {
+  const choiceBlock = document.getElementById(`${fieldId}_choice_block_${index}`)
+  let dragIcon = document.getElementById(`${fieldId}_drag_icon_${index}`)
   
   if (selected == true) {
-    droppable.appendChild(choiceBlock);
-    dragIcon.draggable = true;
-    dragIcon.style.display = 'flex';
+    droppable.appendChild(choiceBlock)
+    dragIcon.draggable = true
+    dragIcon.style.display = 'flex'
   } else {
-    defaultZone.appendChild(choiceBlock);
-    dragIcon.draggable = false;
-    dragIcon.style.display = 'none';
+    defaultZone.appendChild(choiceBlock)
+    dragIcon.draggable = false
+    dragIcon.style.display = 'none'
   }
 }
 
 function toggleAllChoices(selectAllCheckbox) {
-  const selectAllCheckboxId = selectAllCheckbox.id;
-  // selectAllCheckbox.id = fieldId + '_select_all_choice'
-  const choiceIdPrefix = selectAllCheckboxId.replace('_select_all_choice', '');
+  const selectAllCheckboxId = selectAllCheckbox.id
+  const fieldId = selectAllCheckboxId.replace('_checkbox_select_all_choice', '')
   // match all checkboxes of the field, excluding selectAllCheckbox itself and text inputs
   const allChoices = document.querySelectorAll(  
-    `[id^="${choiceIdPrefix}"]:not([id="${selectAllCheckboxId}"],[id^="${choiceIdPrefix}_text"])`
+    `[id^="${fieldId}_checkbox"]:not([id="${selectAllCheckboxId}"],[id^="${fieldId}_text"])`
   )
   allChoices.forEach((choice, i) => {
-    choice.checked = selectAllCheckbox.checked;
+    choice.checked = selectAllCheckbox.checked
     
-    let file_path = document.getElementById(`id_file_path_${i}`);
+    let file_path = document.getElementById(`${fieldId}_file_path_${i}`)
     if (file_path) {
-        file_path.style.display = selectAllCheckbox.checked ? 'flex' : 'none';
+      file_path.style.display = selectAllCheckbox.checked ? 'flex' : 'none'
     }
 
-    let warning_messages = document.getElementById(`id_warnings_${i}`);
+    let warning_messages = document.getElementById(`${fieldId}_warnings_${i}`)
     if (warning_messages) {
-        warning_messages.style.display = selectAllCheckbox.checked ? 'inline' : 'none';
+      warning_messages.style.display = selectAllCheckbox.checked ? 'inline' : 'none'
     }
 
-    let error_messages = document.getElementById(`id_errors_${i}`);
+    let error_messages = document.getElementById(`${fieldId}_errors_${i}`)
     if (error_messages) {
-      error_messages.style.display = selectAllCheckbox.checked ? 'block' : 'none';
+      error_messages.style.display = selectAllCheckbox.checked ? 'block' : 'none'
     }
 
     if (droppable) {
-      moveChoice(droppable, i, selectAllCheckbox.checked);
+      moveChoice(droppable, fieldId, i, selectAllCheckbox.checked)
     }
-  });
+  })
 }
 
 function toggleChoiceAttributesVisibility(checkbox) {
-    const index = checkbox.id.split('_').findLast((e) => e)
-    
-    let file_path = document.getElementById(`id_file_path_${index}`);
-    if (file_path) {
-        file_path.style.display = checkbox.checked ? 'flex' : 'none';
-    }
+  const fieldId = `${checkbox.id.split('_')[0]}_${checkbox.id.split('_')[1]}`
+  const index = checkbox.id.split('_').findLast((e) => e)
 
-    let warning_messages = document.getElementById(`id_warnings_${index}`);
-    if (warning_messages) {
-        warning_messages.style.display = checkbox.checked ? 'inline' : 'none';
-    }
+  let file_path = document.getElementById(`${fieldId}_file_path_${index}`)
+  if (file_path) {
+    file_path.style.display = checkbox.checked ? 'flex' : 'none'
+  }
 
-    let error_messages = document.getElementById(`id_errors_${index}`);
-    if (error_messages) {
-      error_messages.style.display = checkbox.checked ? 'block' : 'none';
-    }
+  let warning_messages = document.getElementById(`${fieldId}_warnings_${index}`)
+  if (warning_messages) {
+    warning_messages.style.display = checkbox.checked ? 'inline' : 'none'
+  }
 
-    if (droppable) {
-      moveChoice(droppable, index, checkbox.checked)
-    }
+  let error_messages = document.getElementById(`${fieldId}_errors_${index}`)
+  if (error_messages) {
+    error_messages.style.display = checkbox.checked ? 'block' : 'none'
+  }
+
+  if (droppable) {
+    moveChoice(droppable, fieldId, index, checkbox.checked)
+  }
 }
 
 function hideChoiceWarningMessages(text) {
-    const index = text.id.split('_').findLast((e) => e)
+  const fieldId = `${text.id.split('_')[0]}_${text.id.split('_')[1]}`
+  const index = text.id.split('_').findLast((e) => e)
 
-    let duration = 1000;
-    clearTimeout(text._timer);
-    text._timer = setTimeout(()=>{
-        let warning_messages = document.getElementById(`id_warnings_${index}`);
-        if (warning_messages) {
-            warning_messages.style.display = 'none';
-        }
-    }, duration);
+  let duration = 1000
+  clearTimeout(text._timer)
+  text._timer = setTimeout(()=>{
+    let warning_messages = document.getElementById(`${fieldId}_warnings_${index}`)
+    if (warning_messages) {
+      warning_messages.style.display = 'none'
+    }
+  }, duration)
 }
 
 draggables.forEach((dragIcon) => {
-  const index = dragIcon.id.replace('drag_icon_', '');
-  let choiceBlock = document.getElementById(index);
+  const fieldId = `${dragIcon.id.split('_')[0]}_${dragIcon.id.split('_')[1]}`
+  const index = dragIcon.id.split('_').findLast((e) => e)
+  let choiceBlock = document.getElementById(`${fieldId}_choice_block_${index}`)
   
   /* DRAG AND DROP */
   dragIcon.addEventListener('dragstart', () => {
-    choiceBlock.classList.add('is-dragging');
-  });
+    choiceBlock.classList.add('is-dragging')
+  })
   dragIcon.addEventListener('dragend', () => {
-    choiceBlock.classList.remove('is-dragging');
-  });
+    choiceBlock.classList.remove('is-dragging')
+  })
 
   /* TOUCH */
   dragIcon.addEventListener('touchstart', () => {
-    const droppablePosition = droppable.getBoundingClientRect();
-    choiceBlock.classList.add('is-dragging');
+    const droppablePosition = droppable.getBoundingClientRect()
+    choiceBlock.classList.add('is-dragging')
     
     dragIcon.addEventListener('touchmove', (eve) => {      
-      eve.preventDefault();
+      eve.preventDefault()
 
-      let nextX = eve.changedTouches[0].clientX;
-      let nextY = eve.changedTouches[0].clientY;
+      let nextX = eve.changedTouches[0].clientX
+      let nextY = eve.changedTouches[0].clientY
 
       if ( // as long as touchmove happens inside of droppable
         nextX >= droppablePosition.left & 
@@ -114,54 +116,54 @@ draggables.forEach((dragIcon) => {
         nextY >= droppablePosition.top &
         nextY <= droppablePosition.bottom
       ) {
-        const bottomChoiceBlock = insertAboveTask(droppable, nextY);
+        const bottomChoiceBlock = insertAboveTask(droppable, nextY)
         
         if (!bottomChoiceBlock) {
-          droppable.appendChild(choiceBlock);
+          droppable.appendChild(choiceBlock)
         } else {
-          droppable.insertBefore(choiceBlock, bottomChoiceBlock);
+          droppable.insertBefore(choiceBlock, bottomChoiceBlock)
         }
       }
-    });
+    })
 
     dragIcon.addEventListener('touchend', () => {
-      choiceBlock.classList.remove('is-dragging');
-    });
+      choiceBlock.classList.remove('is-dragging')
+    })
   
-  });
-});
+  })
+})
 
 
 /* DRAG AND DROP */
 droppable?.addEventListener('dragover', (e) => {
-    e.preventDefault();
+  e.preventDefault()
 
-    const bottomChoiceBlock = insertAboveTask(droppable, e.clientY);
-    const curChoiceBlock = document.querySelector('.is-dragging');
-    
-    if (!bottomChoiceBlock) {
-      droppable.appendChild(curChoiceBlock);
-    } else {
-      droppable.insertBefore(curChoiceBlock, bottomChoiceBlock);
-    }
+  const bottomChoiceBlock = insertAboveTask(droppable, e.clientY)
+  const curChoiceBlock = document.querySelector('.is-dragging')
+  
+  if (!bottomChoiceBlock) {
+    droppable.appendChild(curChoiceBlock)
+  } else {
+    droppable.insertBefore(curChoiceBlock, bottomChoiceBlock)
+  }
 
-});
+})
 
 /* DRAG AND DROP and TOUCH */
 const insertAboveTask = (zone, mouseY) => {
-  const els = zone.querySelectorAll('.choice-block:not(.is-dragging)');
-  let closestChoiceBlock = null;
-  let closestOffset = Number.NEGATIVE_INFINITY;
+  const els = zone.querySelectorAll('.choice-block:not(.is-dragging)')
+  let closestChoiceBlock = null
+  let closestOffset = Number.NEGATIVE_INFINITY
 
   els.forEach((choiceBlock) => {
-    const { top } = choiceBlock.getBoundingClientRect();
-    const offset = mouseY - top;
+    const { top } = choiceBlock.getBoundingClientRect()
+    const offset = mouseY - top
     
     if (offset < 0 && offset > closestOffset) {
-      closestOffset = offset;
-      closestChoiceBlock = choiceBlock;
+      closestOffset = offset
+      closestChoiceBlock = choiceBlock
     }
-  });
+  })
 
-  return closestChoiceBlock;
-};
+  return closestChoiceBlock
+}
